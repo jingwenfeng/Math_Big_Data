49a39e0571382c7f01733fbf61b1de6843eaa3af
jingwenfeng <60762210+jingwenfeng@users.noreply.github.com>
2024-02-06 20:33:47 -0500

    Add files via upload

---
+++ hw2_q5.m
@@ -0,0 +1,26 @@
+A = importdata('SalinasA_corrected.mat');
+[M, N, P] = size(A); 
+A_reshaped = reshape(A, M * N, P);
+[coefficients, score, latent, tsquared, explained] = pca(A_reshaped, 'Economy', false);
+
+%(a)
+disp('(a)')
+disp(coefficients);
+
+%(b)
+sum = cumsum(explained);
+dimensions = find(sum >= 95, 1, 'first');
+disp('(b)')
+disp(dimensions);
+
+%(c)
+disp('(c)')
+disp('First three principal components:');
+disp(coefficients(:, 1:3));
+disp('Last three principal components:');
+disp(coefficients(:, end-2:end));
+disp('Variance explained by the first three principal components:');
+disp(explained(1:3));
+disp('Variance explained by the last three principal components:');
+disp(explained(end-2:end));
+disp('First three include more than 99% of the variance, whereas last three almost nothing');
